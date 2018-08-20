fb1679fa8dfffaf8f5f599d98e3d2de1ee0d5bef
b4rti <b.gorol@efero.de>
2018-08-20 04:30:52 +0200

    Init

--- document.go
+++ document.go
@@ -6,9 +6,19 @@ package poppler
 // #include <glib.h>
 // #include <unistd.h>
 import "C"
+import (
+	"errors"
+	"io"
+	"path/filepath"
+)
 
 type Document struct {
 	doc poppDoc
+}
+
+type DocumentID struct {
+	PermanentID string
+	UpdateID    string
 }
 
 type DocumentInfo struct {
@@ -16,6 +26,46 @@ type DocumentInfo struct {
 	CreationDate, ModificationDate, Pages                                     int
 	IsLinearized                                                              bool
 }
+
+type poppDoc *C.struct__PopplerDocument
+
+func NewDocumentFromPath(path string) (doc *Document, err error) {
+	path, err = filepath.Abs(path)
+	if err != nil {
+		return
+	}
+	var e *C.GError
+	fn := C.g_filename_to_uri((*C.gchar)(C.CString(path)), nil, nil)
+	var d poppDoc
+	d = C.poppler_document_new_from_file((*C.char)(fn), nil, &e)
+	if e != nil {
+		err = errors.New(C.GoString((*C.char)(e.message)))
+	}
+	doc = &Document{
+		doc: d,
+	}
+	return
+}
+
+func NewDocumentFromData(data []byte) (doc *Document, err error)
+
+func NewDocumentFromReader(reader io.Reader) (doc *Document, err error)
+
+func (d *Document) SaveToPath(path string) error
+
+func (d *Document) SaveToData() ([]byte, error)
+
+func (d *Document) SaveToWriter(writer io.Writer) error
+
+func (d *Document) SaveCopyToPath(path string) error
+
+func (d *Document) SaveCopyToData() ([]byte, error)
+
+func (d *Document) SaveCopyToWriter(writer io.Writer) error
+
+func (d *Document) GetID() (DocumentID, error)
+
+func (d *Document) GetVersionString() (DocumentID, error)
 
 func (d *Document) Info() DocumentInfo {
 	return DocumentInfo{
